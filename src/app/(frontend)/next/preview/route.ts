51177ecf6621daf0a82ef3d1b1c61373063b1905
rajeev1503 <rk.rajeevkr1503@gmail.com>
2025-08-06 13:26:50 +0530

    Refactor preview route to streamline error handling and improve parameter validation. Consolidate user authentication and draft mode enabling logic, enhancing overall clarity and maintainability.

--- src/app/(frontend)/next/preview/route.ts
+++ src/app/(frontend)/next/preview/route.ts
@@ -16,69 +16,51 @@ export async function GET(
     };
   } & Request
 ): Promise<Response> {
+  const payload = await getPayload({ config: configPromise });
+
+  const { searchParams } = new URL(req.url);
+
+  const path = searchParams.get("path");
+  //   const collection = searchParams.get('collection') as CollectionSlug
+  const slug = searchParams.get("slug");
+
+  if (!path || !slug) {
+    return new Response("Insufficient search params", { status: 404 });
+  }
+
+  if (!path.startsWith("/")) {
+    return new Response(
+      "This endpoint can only be used for relative previews",
+      { status: 500 }
+    );
+  }
+
+  let user;
+
   try {
-    const payload = await getPayload({ config: configPromise });
-
-    const { searchParams } = new URL(req.url);
-
-    const path = searchParams.get("path");
-    //   const collection = searchParams.get('collection') as CollectionSlug
-    const slug = searchParams.get("slug");
-
-    if (!path || !slug) {
-      return new Response("Insufficient search params", { status: 404 });
-    }
-
-    if (!path.startsWith("/")) {
-      return new Response(
-        "This endpoint can only be used for relative previews",
-        { status: 500 }
-      );
-    }
-
-    let user;
-
-    try {
-      user = await payload.auth({
-        req: req as unknown as PayloadRequest,
-        headers: req.headers,
-      });
-    } catch (error) {
-      payload.logger.error(
-        { err: error },
-        "Error verifying token for live preview"
-      );
-      return new Response("You are not allowed to preview this page", {
-        status: 403,
-      });
-    }
-
-    const draft = await draftMode();
-    console.log("draft", draft, "user", user);
-    if (!user) {
-      draft.disable();
-      return new Response("You are not allowed to preview this page", {
-        status: 403,
-      });
-    }
-
-    // You can add additional checks here to see if the user is allowed to preview this page
-
-    try {
-      draft.enable();
-    } catch (error) {
-      console.error(error);
-      return new Response("Error enabling draft mode", {
-        status: 500,
-      });
-    }
-    console.log("draft", draft.isEnabled);
-    redirect(path);
+    user = await payload.auth({
+      req: req as unknown as PayloadRequest,
+      headers: req.headers,
+    });
   } catch (error) {
-    if (isRedirectError(error)) throw error;
-    console.error(error);
-    return new Response("Error verifying token for live preview", {
-      status: 500,
+    payload.logger.error(
+      { err: error },
+      "Error verifying token for live preview"
+    );
+    return new Response("You are not allowed to preview this page", {
+      status: 403,
     });
   }
+
+  const draft = await draftMode();
+  if (!user) {
+    draft.disable();
+    return new Response("You are not allowed to preview this page", {
+      status: 403,
+    });
+  }
+
+  draft.enable();
+
+  redirect(path);
 }
